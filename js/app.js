40dbc9f1722180703c0e69e2da5ee6ee184421a9
ispicer <iospicer@gmail.com>
2017-09-14 19:53:36 -0400

    Updated portfolio, resume

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ var app = angular.module('app', ['ngRoute']);
 app.config(
   function($routeProvider) {
     $routeProvider
-    	.when('/portraits', {
-        	templateUrl: 'partials/portraits.html',
-    	})
-      	.when('/scenic', {
-      		templateUrl: 'partials/scenic.html',
-      	})
-      	.when('/creative', {
-        	templateUrl: 'partials/creative.html',
-    	});
+      .when('/portraits', {
+          templateUrl: 'partials/portraits.html',
+      })
+        .when('/scenic', {
+          templateUrl: 'partials/scenic.html',
+        })
+        .when('/creative', {
+          templateUrl: 'partials/creative.html',
+      });
       });
